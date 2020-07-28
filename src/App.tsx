8b51d4687919356d838c2307da40ac48dff89f40
George Acosta <georgeacostar@gmail.com>
2020-07-28 15:53:21 -0700

    add hook to toggle todo complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { TodoListItem } from "./TodoListItem";
 
-const todos: Array<Todo> = [
+const initialTodos: Array<Todo> = [
   { text: "get dog food", complete: true },
   { text: "Wash car", complete: false },
 ];
 
-function App() {
+const App: React.FunctionComponent = () => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map((todo) => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete,
+        };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
+
   return (
     <React.Fragment>
       <TodoListItem todo={todos[0]} />
       <TodoListItem todo={todos[1]} />
     </React.Fragment>
   );
-}
+};
 
 export default App;
