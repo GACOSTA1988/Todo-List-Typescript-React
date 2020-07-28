ddca52d29f3f7ee20ba191362020ff6770fc0006
George Acosta <georgeacostar@gmail.com>
2020-07-28 13:10:12 -0700

    add todolistitem.css and react fragment to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { TodoListItem } from "./TodoListItem";
 
 const todos: Array<Todo> = [
-  { text: "get food", complete: true },
+  { text: "get dog food", complete: true },
   { text: "Wash car", complete: false },
 ];
 
 function App() {
   return (
-    <div>
+    <React.Fragment>
       <TodoListItem todo={todos[0]} />
-    </div>
+      <TodoListItem todo={todos[1]} />
+    </React.Fragment>
   );
 }
 
